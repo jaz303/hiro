536aaebd8a23cf937b7e7592711ed4eb1444967e
Jason Frame <jason@onehackoranother.com>
2014-11-07 21:53:06 +0000

    fix application of hierarchy updates

--- queue.js
+++ queue.js
@@ -83,8 +83,8 @@ function create(q) {
 
         scheduled = false;
 
-        hierarchyUpdates.forEach(function(fn, parentComponent, childComponent, targetElement) {
-            fn(parentComponent, childComponent, targetElement);
+        hierarchyUpdates.forEach(function(op) {
+            op[0](op[1], op[2], op[3]);
         });
         hierarchyUpdates.clear();
 
